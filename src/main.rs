fe886a35e3c0e8d4e9351eceb9903b1f6eb1d3de
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-04-29 11:17:03 +1000

    Sketch out some of the core algorithm.

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ use std::fs::File;
 /// Minimum resolution for our floating-point comparisons.
 const EPSILON: f64 = 1e-7;
 
-/// Gamma constant for RGBA conversion.
+/// Gamma constant for image conversion.
 const GAMMA: f64 = 2.2;
 
 /// The maximum number of recursive traces we can perform when rendering the image.
 const MAX_TRACE_DEPTH: usize = 3;
 
-// We use cgmath for it's excellent vector types.
 type Point = Vector3<f64>;
 type Vector = Vector3<f64>;
 
@@ -33,14 +32,6 @@ fn clamp<V: PartialOrd>(value: V, lower: V, upper: V) -> V {
 /// Converts the given value to radians from degrees.
 fn to_radians(degrees: f64) -> f64 {
 	degrees * (PI / 180.0)
-}
-
-fn encode_gamma(value: f64) -> u8 {
-	(value).powf(1.0 / GAMMA) as u8
-}
-
-fn decode_gamma(value: u8) -> f64 {
-	(value as f64).powf(GAMMA)
 }
 
 /// Defines a color in floating-point RGBA color space.
@@ -86,6 +77,10 @@ impl Image {
 
 	/// Loads a .PNG image from the given path.
 	pub fn load(path: &str) -> Image {
+		fn decode_gamma(value: u8) -> f64 {
+			(value as f64).powf(GAMMA)
+		}
+
 		unimplemented!()
 	}
 
@@ -107,6 +102,10 @@ impl Image {
 
 	/// Saves the image in .PNG format to the given path.
 	pub fn save(&self, path: &str) {
+		fn encode_gamma(value: f64) -> u8 {
+			(value).powf(1.0 / GAMMA) as u8
+		}
+
 		unimplemented!()
 	}
 }
@@ -264,7 +263,7 @@ impl Scene {
 
 		for y in 0..height {
 			for x in 0..width {
-				let camera_ray = self.project(x, y, width, height);
+				let camera_ray = self.project(x as f64, y as f64, width as f64, height as f64);
 				let color = self.trace(&camera_ray, 0, MAX_TRACE_DEPTH);
 
 				image.set(x, y, color.clamp());
@@ -272,11 +271,6 @@ impl Scene {
 		}
 
 		image
-	}
-
-	/// Projects a ray into the scene at the given coordinates.
-	fn project(&self, x: usize, y: usize, width: usize, height: usize) -> Ray {
-		unimplemented!()
 	}
 
 	/// Samples the color at the resultant object by projecting a ray into the scene
@@ -288,12 +282,86 @@ impl Scene {
 			return self.background_color.clone();
 		}
 
-		unimplemented!()
+		// project a ray into the scene for each pixel in our resultant image
+		let intersection = self.find_intersecting_object(ray);
+
+		// if we're able to locate a valid intersection for this ray
+		if let Some(intersection) = intersection {
+			let (node, distance) = intersection;
+			let material = node.material();
+
+			// calculate the hit point, normal and UV on the surface of the object
+			let hit_point = ray.origin + ray.direction * distance;
+			let surface_normal = node.calculate_normal(&hit_point);
+			let surface_uv = node.calculate_uv(&hit_point);
+
+			// apply lighting and shading to the object
+			let mut color = self.apply_diffuse_shading(distance, &material, &hit_point, &surface_normal, &surface_uv);
+
+			// TODO: apply reflectivity
+			// TODO: apply refractivity
+
+			return color;
+		}
+
+		// otherwise, sample the background color
+		self.background_color.clone()
 	}
 
-	/// Traces a ray into the scene, attempting to find the first intersecting object that it collides with
-	fn find_intersection_object(&self, ray: &Ray) -> Option<(&SceneNode, f64)> {
-		unimplemented!()
+	/// Applies lighting to an object's material by evaluating all the lights in the scene.
+	fn apply_diffuse_shading(&self,
+													 distance: f64,
+													 material: &Material,
+													 hit_point: &Point,
+													 surface_normal: &Vector,
+													 surface_uv: &UV) -> Color {
+		let mut color = Color::BLACK;
+		let albedo = material.sample(surface_uv);
+
+		// walk through all lights in the scene
+		for light in self.lights.iter() {
+			unimplemented!()
+		}
+
+		color
+	}
+
+	/// Traces a ray into the scene, attempting to find the first intersecting object that it collides with.
+	fn find_intersecting_object(&self, ray: &Ray) -> Option<(&Box<SceneNode>, f64)> {
+		let mut distance = 999999999.0;
+		let mut result: Option<(&Box<SceneNode>, f64)> = None;
+
+		// walk through all nodes in the scene
+		for node in self.nodes.iter() {
+			let intersection = node.intersects(ray);
+
+			// if our ray intersects with the node
+			if intersection.is_some() {
+				let hit_distance = intersection.unwrap();
+
+				// and the intersection point is the closest we've located so far
+				if hit_distance < distance {
+					distance = hit_distance;
+					result = Some((node, distance)) // then record the result
+				}
+			}
+		}
+
+		result
+	}
+
+	/// Projects a ray into the scene at the given coordinates.
+	fn project(&self, x: f64, y: f64, width: f64, height: f64) -> Ray {
+		assert!(width > height);
+
+		let fov_adjustment = (to_radians(self.field_of_view) / 2.0).tan();
+		let aspect_ratio = width / height;
+		let sensor_x = ((((x + 0.5) / width) * 2.0 - 1.0) * aspect_ratio) * fov_adjustment;
+		let sensor_y = (1.0 - ((y + 0.5) / height) * 2.0) * fov_adjustment;
+
+		let direction = vec3(sensor_x, sensor_y, -1.0).normalize();
+
+		Ray { origin: vec3(0.0, 0.0, 0.0), direction }
 	}
 }
 
@@ -366,7 +434,9 @@ fn main() {
 		..Default::default()
 	};
 
+	// render at a decent resolution
 	let image = scene.render(1920, 1080);
 
+	// and save to .PNG
 	image.save("output.png");
 }
