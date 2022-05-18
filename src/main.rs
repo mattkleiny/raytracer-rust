3d20ebb232af7f361d21978fdbf68ea9de718627
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-05-19 08:53:09 +1000

    Implement basic math routines for tuples.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![allow(dead_code)]
+
+use math::*;
+
+mod math;
+
 fn main() {
-  print!("Hello, World!");
+  let a = vec(2., 3., 4.);
+  let b = vec(1., -6., 8.);
+
+  print!("a * b = {:?}", a + b);
 }
---
+++ src/math.rs
@@ -0,0 +1,16 @@
+//! Standard mathematics and related linear algebra for implementing our ray tracer.
+
+pub use tuples::*;
+
+mod tuples;
+
+/// Determines that two values are approximately equal.
+pub trait ApproxEq<Rhs = Self> where Self: Copy {
+  fn is_approx(self, rhs: Rhs) -> bool;
+}
+
+impl ApproxEq for f32 {
+  fn is_approx(self, rhs: Self) -> bool {
+    (self - rhs).abs() < f32::EPSILON
+  }
+}
